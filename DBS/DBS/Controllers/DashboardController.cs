5b211ff0164470ba6ad4f8feaf25793de5f741d0
Enzo Torelli <enzo.torelli461@gmail.com>
2026-03-25 15:51:19 -0300

    dashboard com side bar, layour e css organizados

---
+++ DBS/DBS/Controllers/DashboardController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DBS.Controllers
+{
+    public class DashboardController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Dashboard";
+            ViewData["Pagina"] = "Dashboard";
+            return View();
+        }
+    }
+}
--- DBS/DBS/Controllers/LoginController.cs
+++ DBS/DBS/Controllers/LoginController.cs
@@ -4,8 +4,21 @@ namespace DBS.Controllers
 {
     public class LoginController : Controller
     {
+        // Abre a tela de login
         public IActionResult Index()
         {
+            return View();
+        }
+
+        // Recebe os dados do formulário
+        [HttpPost]
+        public IActionResult Index(string email, string senha)
+        {
+            if (email == "admin" && senha == "123")
+            {
+                return RedirectToAction("Index", "Dashboard");
+            }
+
             return View();
         }
     }
