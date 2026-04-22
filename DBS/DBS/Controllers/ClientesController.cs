442e8a7c14426a0f4e56f3f6456bc83ccbdcc2ee
Enzo Torelli <enzo.torelli461@gmail.com>
2026-04-22 14:55:06 -0300

    Feito as páginas de clientes e os produtos

---
+++ DBS/DBS/Controllers/ClientesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DBS.Controllers
+{
+    public class ClientesController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Cliente";
+            ViewData["Pagina"] = "Clientes";
+            return View();
+        }
+    }
+}
---
+++ DBS/DBS/Controllers/ProdutosController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DBS.Controllers
+{
+    public class ProdutosController : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Produtos";
+            ViewData["Pagina"] = "Produtos";
+            return View();
+        }
+    }
+}
