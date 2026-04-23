f7b5ef5c8504df79890c58703fe72e5e1289827a
Franklin Artur <franklin.artur@tragetta.com.br>
2026-04-23 16:12:18 -0300

    Feito aba Ordens e Vendas

---
+++ DBS/DBS/Controllers/OrdensController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace DBS.Controllers
+{
+    public class OrdensController : Controller
+    {
+        public IActionResult Index()
+        {
+            var ordens = new List<Ordem>
+            {
+                new Ordem { Id = 4521, ClienteNome = "Ana Costa", Valor = 890, Status = "Pago" },
+                new Ordem { Id = 4520, ClienteNome = "Carlos Souza", Valor = 120, Status = "Pendente" }
+            };
+
+            return View(ordens);
+        }
+    }
+
+    public class Ordem
+    {
+        public int Id { get; set; }
+        public string ClienteNome { get; set; }
+        public decimal Valor { get; set; }
+        public string Status { get; set; }
+    }
+}
---
+++ DBS/DBS/Controllers/VendasController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+
+namespace DBS.Controllers
+{
+    public class VendasController : Controller
+    {
+        public IActionResult Index()
+        {
+            var vendas = new List<Venda>
+            {
+                new Venda { Id = 1001, ClienteNome = "Maria Silva", Valor = 320, Data = DateTime.Now, Status = "Pago" },
+                new Venda { Id = 1002, ClienteNome = "João Pereira", Valor = 150, Data = DateTime.Now, Status = "Pendente" }
+            };
+
+            return View(vendas);
+        }
+    }
+
+    public class Venda
+    {
+        public int Id { get; set; }
+        public string ClienteNome { get; set; }
+        public decimal Valor { get; set; }
+        public DateTime Data { get; set; }
+        public string Status { get; set; }
+    }
+}
